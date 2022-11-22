329bb485bbf8d0fd327b994c9950a4b31560c12c
ruben carmona <carmona.algaba.ruben@iescamas.es>
2022-11-22 21:46:29 +0100

    Modificación del archivo Config.php y subida de los demás archivos del controlador

---
+++ app/Config.php
@@ -0,0 +1,11 @@
+<?php
+
+ class Config
+ {
+     static public $mvc_bd_hostname = "localhost";
+     static public $mvc_bd_nombre   = "alimentos42022";
+     static public $mvc_bd_usuario  = "myjosloploa9";
+     static public $mvc_bd_clave    = "6Jlhs72S";
+     static public $mvc_vis_css     = "estilo.css";
+ }
+?>
